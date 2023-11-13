e9af337ec3c52cc2e3bf5a4951b94f2b2ffc93a2
Jefferson Moraes <jeffersonjuniormoraes@gmail.com>
2023-11-13 10:53:57 -0300

    :tada: First Commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import symbolOn from "./assets/images/symbol-on.png";
+import symbolOff from "./assets/images/symbol-off.png";
 
 export default function App() {
+  const [isActive, setIsActive] = useState(true);
+
+  function handleSymbol() {
+    setIsActive(!isActive);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={handleSymbol}>
+        <Image source={isActive ? symbolOn : symbolOff} />
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOn: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  containerOff: {
+    flex: 1,
+    backgroundColor: "#e4e4e4",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
